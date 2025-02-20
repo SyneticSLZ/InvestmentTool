07ef757b81210f18c1c4b657023bc00266732664
Synetic1 <syneticslz@gmail.com>
2025-02-20 15:02:24 +0000

    f

--- producthunt-view.js
+++ producthunt-view.js
@@ -3517,7 +3517,7 @@ function setupEventListeners() {
             tab.classList.remove('text-gray-500', 'hover:text-gray-700');
             
             // Update content
-            activeModalTab = tabName;
+            factiveModalTab = tabName;
             const event = fundingData.find(e => e.uuid === currentEventId);
             if (event) {
                 await updateModalContentfunding(event);
